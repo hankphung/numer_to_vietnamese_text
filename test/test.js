45ddee1c984f268bac2d58d008729ce264f62a29
Phùng Đông Hưng (Hank) <donghung.viethanit@gmail.com>
2025-08-27 17:51:48 +0200

    updated test

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ var assert = require('assert');
 var to_vietnamese = require('../src/n2vi');
 describe('Number to Vietnamese text', function() {
    it("should convert up to hungreds of billion ", function() {
-      assert.equal(to_vietnamese(120020120100),'một trăm hai mươi tỉ không trăm hai mươi triệu một trăm hai mươi nghìn một trăm')
+      assert.equal(to_vietnamese(120020120100),'một trăm hai mươi tỷ không trăm hai mươi triệu một trăm hai mươi nghìn một trăm')
    })
 
    it("should convert 1 to 9",function(){
-      assert.equal(to_vietnamese(12345678900),"mười hai tỉ ba trăm bốn mươi lăm triệu sáu trăm bảy mươi tám nghìn chín trăm")
+      assert.equal(to_vietnamese(12345678900),"mười hai tỷ ba trăm bốn mươi lăm triệu sáu trăm bảy mươi tám nghìn chín trăm")
    })
 
    it("should flxsexible in 05 and 21",function(){
@@ -21,12 +21,12 @@ describe('Number to Vietnamese text', function() {
       assert.equal(to_vietnamese(21000000,'đồng'),"hai mươi mốt triệu đồng")
    })
    it("should beable to handle of billions", function(){
-      assert.equal(to_vietnamese(21000000000,'đồng'),"hai mươi mốt tỉ đồng")
+      assert.equal(to_vietnamese(21000000000,'đồng'),"hai mươi mốt tỷ đồng")
    })
    it("should beable to handle even thousand of billions", function(){
-      assert.equal(to_vietnamese(210000000000001,'đồng'),"hai trăm mười nghìn tỉ không trăm lẻ một đồng")
+      assert.equal(to_vietnamese(210000000000001,'đồng'),"hai trăm mười nghìn tỷ không trăm lẻ một đồng")
    })
    it("should beable to handle thousands of billions", function(){
-      assert.equal(to_vietnamese(1230950880055,'đồng'),"một nghìn hai trăm ba mươi tỉ chín trăm năm mươi triệu tám trăm tám mươi nghìn không trăm năm mươi lăm đồng")
+      assert.equal(to_vietnamese(1230950880055,'đồng'),"một nghìn hai trăm ba mươi tỷ chín trăm năm mươi triệu tám trăm tám mươi nghìn không trăm năm mươi lăm đồng")
    })
 })
